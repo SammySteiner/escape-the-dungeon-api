64051a260c22a6b066c2fa7a40d7f61c57d20fc1
Sammy Steiner <sammysteiner@gmail.com>
2017-06-02 02:14:31 -0400

    fixed a bug that put the door on the same coord with one monster

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -6,12 +6,14 @@ class Api::V1::BoardsController < ApplicationController
   end
 
   def create
-    random_coords = generate_random_coords(board_params[:size].to_i, 4)
+    random_coords = generate_random_coords(board_params[:size].to_i, 3 + board_params[:monsters].to_i)
     board = Board.create(name: board_params[:name], size: board_params[:size])
     player = Player.create(x: random_coords[0][0], y: random_coords[0][1], hearts: 1, board: board)
     key = Key.create(x: random_coords[1][0], y: random_coords[1][1], board: board)
     door = Door.create(x: random_coords[2][0], y: random_coords[2][1], board: board)
-    monster = Monster.create(x: random_coords[3][0], y: random_coords[3][1], board: board)
+    board_params[:monsters].to_i.times do |i|
+      monster = Monster.create(x: random_coords[3 + i][0], y: random_coords[3 + i][1], board: board)
+    end
     render json: board
   end
 
@@ -24,9 +26,15 @@ class Api::V1::BoardsController < ApplicationController
     render json: board
   end
 
-  def delete
+  def destroy
     board = Board.find_by(name: params[:id])
-    board.destroy_all
+    board.player.destroy
+    board.key.destroy
+    board.door.destroy
+    board.monsters.each do |monster|
+      monster.destroy
+    end
+    board.destroy
     render json: board
   end
 
@@ -38,7 +46,7 @@ class Api::V1::BoardsController < ApplicationController
   private
 
   def board_params
-    params.require(:board).permit(:name, :size, :board)
+    params.require(:board).permit(:name, :size, :monsters)
   end
 
   def generate_random_coords(grid_size, number_of_coords)
